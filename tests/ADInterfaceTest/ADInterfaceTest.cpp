bdc383afbba1ce5b4b80f9177a0e49399431c0a4
Dustin Condon <dustin.james.condon@gmail.com>
2023-12-04 21:09:15 -0800

    Adjust unit test proj

--- tests/ADInterfaceTest/ADInterfaceTest.cpp
+++ tests/ADInterfaceTest/ADInterfaceTest.cpp
@@ -1,7 +1,11 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "FASTInterface.h"
+#include "AeroDyn_Interface_Wrapper.h"
+#include "Eigen/Sparse"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Eigen;
 
 namespace ADInterfaceTest
 {
@@ -11,6 +15,27 @@ namespace ADInterfaceTest
 		
 		TEST_METHOD(TestMethod1)
 		{
+			AeroDyn_Interface_Wrapper ad_interface;
+			double dt = 0.05;
+			auto hubPos = Vector3d::Zero();
+			auto hubOri = Matrix3d::Identity();
+			auto hubVel = Vector3d::Zero();
+			auto hubAcc = Vector3d::Zero();
+			auto hubRotVel = Vector3d::Zero();
+			auto hubRotAcc = Vector3d::Zero();
+			double bladePitch = 0.0;
+
+			ad_interface.InitAerodyn("../resources/5MW_OC4Semi_WSt_WavesWN/NRELOffshrBsline5MW_OC3Hywind_AeroDyn15.dat",
+				"output",
+				dt, 3, 1, 0, false, 0.0,
+				hubPos,
+				hubOri,
+				hubVel,
+				hubAcc,
+				hubRotVel,
+				hubRotAcc,
+				bladePitch
+			);
 		}
 	};
 }
